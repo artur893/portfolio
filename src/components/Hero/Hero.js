e15220eeb29e64c0d8d43a6653b89733a0b29f5f
artur893 <arthal893@gmail.com>
2023-02-14 15:40:43 +0100

    Projects

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,11 +5,11 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = () => (
-  <Section>
+  <Section row nopadding>
     <LeftSection>
       <SectionTitle main>
-        Welcome to <br />
-        my portfolio
+        Welcome To <br />
+        My Personal Portfolio
       </SectionTitle>
       <SectionText>Modern, professional front-end developer</SectionText>
       <Button>Learn more</Button>
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -3,10 +3,14 @@ import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+  <Section id='tech'>
+    <SectionDivider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I have worked with a range of Front-End technologies in the web development world.
+    </SectionText>
+  </Section>
 );
 
 export default Technologies;
